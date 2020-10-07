f322c8c927835526f00136d70f54b4363aaab6b8
Wei Hu <weh@microsoft.com>
2020-10-07 15:16:39 -0600

    PCI: hv: Fix the PCI HyperV probe failure path to release resource properly
    BugLink: https://bugs.launchpad.net/bugs/1883261
    
    In some error cases in hv_pci_probe(), allocated resources are not freed.
    
    Fix this by adding a field to keep track of the high water mark for slots
    that have resources allocated to them.  In case of an error, this high
    water mark is used to know which slots have resources that must be released.
    Since slots are numbered starting with zero, a value of -1 indicates no
    slots have been allocated resources.  There may be unused slots in the range
    between slot 0 and the high water mark slot, but these slots are already
    ignored by the existing code in the allocate and release loops with the call
    to get_pcichild_wslot().
    
    Link: https://lore.kernel.org/r/20200507050211.10923-1-weh@microsoft.com
    Signed-off-by: Wei Hu <weh@microsoft.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Reviewed-by: Michael Kelley <mikelley@microsoft.com>
    (cherry picked from commit 83cc3508ffaa6e2cd364d29418d35fab6f069b51)
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: William Breathitt Gray <william.gray@canonical.com>

--- drivers/pci/controller/pci-hyperv.c
+++ drivers/pci/controller/pci-hyperv.c
@@ -519,6 +519,9 @@ struct hv_pcibus_device {
 
 	struct workqueue_struct *wq;
 
+	/* Highest slot of child device with resources allocated */
+	int wslot_res_allocated;
+
 	/* hypercall arg, must not cross page boundary */
 	struct retarget_msi_interrupt retarget_msi_interrupt_params;
 
@@ -2899,7 +2902,7 @@ static int hv_send_resources_allocated(struct hv_device *hdev)
 	struct hv_pci_dev *hpdev;
 	struct pci_packet *pkt;
 	size_t size_res;
-	u32 wslot;
+	int wslot;
 	int ret;
 
 	size_res = (pci_protocol_version < PCI_PROTOCOL_VERSION_1_2)
@@ -2952,6 +2955,8 @@ static int hv_send_resources_allocated(struct hv_device *hdev)
 				comp_pkt.completion_status);
 			break;
 		}
+
+		hbus->wslot_res_allocated = wslot;
 	}
 
 	kfree(pkt);
@@ -2970,10 +2975,10 @@ static int hv_send_resources_released(struct hv_device *hdev)
 	struct hv_pcibus_device *hbus = hv_get_drvdata(hdev);
 	struct pci_child_message pkt;
 	struct hv_pci_dev *hpdev;
-	u32 wslot;
+	int wslot;
 	int ret;
 
-	for (wslot = 0; wslot < 256; wslot++) {
+	for (wslot = hbus->wslot_res_allocated; wslot >= 0; wslot--) {
 		hpdev = get_pcichild_wslot(hbus, wslot);
 		if (!hpdev)
 			continue;
@@ -2988,7 +2993,11 @@ static int hv_send_resources_released(struct hv_device *hdev)
 				       VM_PKT_DATA_INBAND, 0);
 		if (ret)
 			return ret;
+
+		hbus->wslot_res_allocated = wslot - 1;
 	}
+
+	hbus->wslot_res_allocated = -1;
 
 	return 0;
 }
@@ -3071,6 +3080,7 @@ static int hv_pci_probe(struct hv_device *hdev,
 	if (!hbus)
 		return -ENOMEM;
 	hbus->state = hv_pcibus_init;
+	hbus->wslot_res_allocated = -1;
 
 	/*
 	 * The PCI bus "domain" is what is called "segment" in ACPI and other
@@ -3170,7 +3180,7 @@ static int hv_pci_probe(struct hv_device *hdev,
 
 	ret = hv_pci_allocate_bridge_windows(hbus);
 	if (ret)
-		goto free_irq_domain;
+		goto exit_d0;
 
 	ret = hv_send_resources_allocated(hdev);
 	if (ret)
@@ -3188,6 +3198,8 @@ static int hv_pci_probe(struct hv_device *hdev,
 
 free_windows:
 	hv_pci_free_bridge_windows(hbus);
+exit_d0:
+	(void) hv_pci_bus_exit(hdev, true);
 free_irq_domain:
 	irq_domain_remove(hbus->irq_domain);
 free_fwnode:
