32ad2f643c613f96d4a53e7d7875442c51a9ccbd
Andrea Parri (Microsoft) <parri.andrea@gmail.com>
2021-02-03 12:35:12 +0100

    UBUNTU: SAUCE: hv_netvsc: Allocate the recv_buf buffers after NVSP_MSG1_TYPE_SEND_RECV_BUF
    BugLink: https://bugs.launchpad.net/bugs/1915146
    
    The recv_buf buffers are allocated in netvsc_device_add().  Later in
    netvsc_init_buf() the response to NVSP_MSG1_TYPE_SEND_RECV_BUF allows
    the host to set up a recv_section_size that could be bigger than the
    (default) value used for that allocation.  The host-controlled value
    could be used by a malicious host to bypass the check on the packet's
    length in netvsc_receive() and hence to overflow the recv_buf buffer.
    
    Move the allocation of the recv_buf buffers into netvsc_init_but().
    
    Reported-by: Juan Vazquez <juvazq@microsoft.com>
    Signed-off-by: Andrea Parri (Microsoft) <parri.andrea@gmail.com>
    Fixes: 0ba35fe91ce34f ("hv_netvsc: Copy packets sent by Hyper-V out of the receive buffer")
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (backported from commit 0102eeedb71757d6589144cf019424f69b3ab289
     from git://git.kernel.org/pub/scm/linux/kernel/git/netdev/net-next.git)
    [marcelo.cerri@canonical.com: minor context adjustments]
    Signed-off-by: Marcelo Henrique Cerri <marcelo.cerri@canonical.com>

--- drivers/net/hyperv/netvsc.c
+++ drivers/net/hyperv/netvsc.c
@@ -337,7 +337,7 @@ static int netvsc_init_buf(struct hv_device *device,
 	unsigned int buf_size;
 	unsigned long vaddr;
 	size_t map_words;
-	int ret = 0, i;
+	int i, ret = 0;
 
 	/* Get receive buffer area. */
 	buf_size = device_info->recv_sections * device_info->recv_section_size;
@@ -452,6 +452,16 @@ static int netvsc_init_buf(struct hv_device *device,
 			   net_device->recv_section_size);
 		ret = -EINVAL;
 		goto cleanup;
+	}
+
+	for (i = 0; i < VRSS_CHANNEL_MAX; i++) {
+		struct netvsc_channel *nvchan = &net_device->chan_table[i];
+
+		nvchan->recv_buf = kzalloc(net_device->recv_section_size, GFP_KERNEL);
+		if (nvchan->recv_buf == NULL) {
+			ret = -ENOMEM;
+			goto cleanup;
+		}
 	}
 
 	/* Setup receive completion ring.
@@ -1611,12 +1621,6 @@ struct netvsc_device *netvsc_device_add(struct hv_device *device,
 
 	for (i = 0; i < VRSS_CHANNEL_MAX; i++) {
 		struct netvsc_channel *nvchan = &net_device->chan_table[i];
-
-		nvchan->recv_buf = kzalloc(device_info->recv_section_size, GFP_KERNEL);
-		if (nvchan->recv_buf == NULL) {
-			ret = -ENOMEM;
-			goto cleanup2;
-		}
 
 		nvchan->channel = device->channel;
 		nvchan->net_device = net_device;
