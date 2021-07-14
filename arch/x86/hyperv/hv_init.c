fe8966d75d9b605505b52bb90d2a1fcb51900bad
Michael Kelley <mikelley@microsoft.com>
2021-07-14 11:34:47 -0700

    Drivers: hv: Move Hyper-V misc functionality to arch-neutral code
    BugLink: https://bugs.launchpad.net/bugs/1949770
    
    The check for whether hibernation is possible, and the enabling of
    Hyper-V panic notification during kexec, are both architecture neutral.
    Move the code from under arch/x86 and into drivers/hv/hv_common.c where
    it can also be used for ARM64.
    
    No functional change.
    
    Signed-off-by: Michael Kelley <mikelley@microsoft.com>
    Link: https://lore.kernel.org/r/1626287687-2045-4-git-send-email-mikelley@microsoft.com
    Signed-off-by: Wei Liu <wei.liu@kernel.org>
    (backported from commit 6dc77fa5ac2cf26f846a51492dbe42526e26d0f2)
    [rtg - context differences, dropped #include <linux/panic_notifier.h>]
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Juerg Haefliger <juerg.haefliger@canonical.com>
    Acked-by: Joseph Salisbury <joseph.salisbury@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- arch/x86/hyperv/hv_init.c
+++ arch/x86/hyperv/hv_init.c
@@ -7,9 +7,9 @@
  * Author : K. Y. Srinivasan <kys@microsoft.com>
  */
 
-#include <linux/acpi.h>
 #include <linux/efi.h>
 #include <linux/types.h>
+#include <linux/io.h>
 #include <asm/apic.h>
 #include <asm/desc.h>
 #include <asm/hypervisor.h>
@@ -464,8 +464,3 @@ bool hv_is_hyperv_initialized(void)
 }
 EXPORT_SYMBOL_GPL(hv_is_hyperv_initialized);
 
-bool hv_is_hibernation_supported(void)
-{
-	return acpi_sleep_state_supported(ACPI_STATE_S4);
-}
-EXPORT_SYMBOL_GPL(hv_is_hibernation_supported);
