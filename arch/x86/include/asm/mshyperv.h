57fb9461be99179a353a11cc0c1270d4c1714891
Michael Kelley <mikelley@microsoft.com>
2021-07-14 11:34:45 -0700

    Drivers: hv: Make portions of Hyper-V init code be arch neutral
    BugLink: https://bugs.launchpad.net/bugs/1965618
    
    The code to allocate and initialize the hv_vp_index array is
    architecture neutral. Similarly, the code to allocate and
    populate the hypercall input and output arg pages is architecture
    neutral.  Move both sets of code out from arch/x86 and into
    utility functions in drivers/hv/hv_common.c that can be shared
    by Hyper-V initialization on ARM64.
    
    No functional changes. However, the allocation of the hypercall
    input and output arg pages is done differently so that the
    size is always the Hyper-V page size, even if not the same as
    the guest page size (such as with ARM64's 64K page size).
    
    Signed-off-by: Michael Kelley <mikelley@microsoft.com>
    Link: https://lore.kernel.org/r/1626287687-2045-2-git-send-email-mikelley@microsoft.com
    Signed-off-by: Wei Liu <wei.liu@kernel.org>
    (backported from commit afca4d95dd7d7936d46a0ff02169cc40f534a6a3)
    [Boqun - many context adjustments]
    Signed-off-by: Boqun Feng <Boqun.Feng@microsoft.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Joseph Salisbury <joseph.salisbury@canonical.com>
    Acked-by: Philip Cox <philip.cox@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- arch/x86/include/asm/mshyperv.h
+++ arch/x86/include/asm/mshyperv.h
@@ -39,7 +39,6 @@ void hv_stimer0_callback_vector(void);
 
 #if IS_ENABLED(CONFIG_HYPERV)
 extern void *hv_hypercall_pg;
-extern void  __percpu  **hyperv_pcpu_input_arg;
 
 static inline u64 hv_do_hypercall(u64 control, void *input, void *output)
 {
