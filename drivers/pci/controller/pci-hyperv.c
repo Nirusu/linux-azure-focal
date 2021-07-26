30f029db377af64692ab2c96eb00048f7865b860
Boqun Feng <boqun.feng@gmail.com>
2021-07-27 02:06:55 +0800

    PCI: hv: Set ->domain_nr of pci_host_bridge at probing time
    BugLink: https://bugs.launchpad.net/bugs/1949770
    
    No functional change, just store and maintain the PCI domain number in
    the ->domain_nr of pci_host_bridge. Note that we still need to keep
    the copy of domain number in x86-specific pci_sysdata, because x86 is
    not a PCI_DOMAINS_GENERIC=y architecture, so the ->domain_nr of
    pci_host_bridge doesn't work for it yet.
    
    Link: https://lore.kernel.org/r/20210726180657.142727-7-boqun.feng@gmail.com
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    (cherry picked from commit 38c0d266dc80b81f7f72314620f01ff6a1e119fe)
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Ian May <ian.may@canonical.com>
    Acked-by: Joseph Salisbury <joseph.salisbury@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/pci/controller/pci-hyperv.c
+++ drivers/pci/controller/pci-hyperv.c
@@ -2366,7 +2366,7 @@ static void hv_eject_device_work(struct work_struct *work)
 	 * because hbus->bridge->bus may not exist yet.
 	 */
 	wslot = wslot_to_devfn(hpdev->desc.win_slot.slot);
-	pdev = pci_get_domain_bus_and_slot(hbus->sysdata.domain, 0, wslot);
+	pdev = pci_get_domain_bus_and_slot(hbus->bridge->domain_nr, 0, wslot);
 	if (pdev) {
 		pci_lock_rescan_remove();
 		pci_stop_and_remove_bus_device(pdev);
@@ -3152,6 +3152,7 @@ static int hv_pci_probe(struct hv_device *hdev,
 			 "PCI dom# 0x%hx has collision, using 0x%hx",
 			 dom_req, dom);
 
+	hbus->bridge->domain_nr = dom;
 	hbus->sysdata.domain = dom;
 
 	hbus->hdev = hdev;
@@ -3163,7 +3164,7 @@ static int hv_pci_probe(struct hv_device *hdev,
 	spin_lock_init(&hbus->retarget_msi_interrupt_lock);
 	init_completion(&hbus->remove_event);
 	hbus->wq = alloc_ordered_workqueue("hv_pci_%x", 0,
-					   hbus->sysdata.domain);
+					   hbus->bridge->domain_nr);
 	if (!hbus->wq) {
 		ret = -ENOMEM;
 		goto free_dom;
@@ -3290,7 +3291,7 @@ close:
 destroy_wq:
 	destroy_workqueue(hbus->wq);
 free_dom:
-	hv_put_dom_num(hbus->sysdata.domain);
+	hv_put_dom_num(hbus->bridge->domain_nr);
 free_bus:
 	kfree(hbus);
 	return ret;
@@ -3407,7 +3408,7 @@ static int hv_pci_remove(struct hv_device *hdev)
 	put_hvpcibus(hbus);
 	wait_for_completion(&hbus->remove_event);
 
-	hv_put_dom_num(hbus->sysdata.domain);
+	hv_put_dom_num(hbus->bridge->domain_nr);
 
 	kfree(hbus);
 	return ret;
