28a4158e5ca135e53cff8650a9299ea3393207b9
Boqun Feng <boqun.feng@gmail.com>
2021-07-27 02:06:56 +0800

    PCI: hv: Set up MSI domain at bridge probing time
    BugLink: https://bugs.launchpad.net/bugs/1949770
    
    Since PCI_HYPERV depends on PCI_MSI_IRQ_DOMAIN which selects
    GENERIC_MSI_IRQ_DOMAIN, we can use dev_set_msi_domain() to set up the
    MSI domain at probing time, and this works for both x86 and ARM64.
    
    Therefore use it as the preparation for ARM64 Hyper-V PCI support.
    
    As a result, no longer need to maintain ->fwnode in x86 specific
    pci_sysdata, and make hv_pcibus_device own it instead.
    
    Link: https://lore.kernel.org/r/20210726180657.142727-8-boqun.feng@gmail.com
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    (backported from commit 9e7f9178ab4943b3a7294a12bc38925c515ca3f0)
    [rtg - minor context adjustment]
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Juerg Haefliger <juerg.haefliger@canonical.com>
    Acked-by: Joseph Salisbury <joseph.salisbury@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/pci/controller/pci-hyperv.c
+++ drivers/pci/controller/pci-hyperv.c
@@ -455,6 +455,7 @@ struct hv_pcibus_device {
 	struct pci_config_window sysdata;
 #endif
 	struct pci_host_bridge *bridge;
+	struct fwnode_handle *fwnode;
 	/* Protocol version negotiated with the host */
 	enum pci_protocol_version_t protocol_version;
 	enum hv_pcibus_state state;
@@ -1576,7 +1577,7 @@ static int hv_pcie_init_irq_domain(struct hv_pcibus_device *hbus)
 	hbus->msi_info.handler = handle_edge_irq;
 	hbus->msi_info.handler_name = "edge";
 	hbus->msi_info.data = hbus;
-	hbus->irq_domain = pci_msi_create_irq_domain(hbus->sysdata.fwnode,
+	hbus->irq_domain = pci_msi_create_irq_domain(hbus->fwnode,
 						     &hbus->msi_info,
 						     x86_vector_domain);
 	if (!hbus->irq_domain) {
@@ -1584,6 +1585,8 @@ static int hv_pcie_init_irq_domain(struct hv_pcibus_device *hbus)
 			"Failed to build an MSI IRQ domain\n");
 		return -ENODEV;
 	}
+
+	dev_set_msi_domain(&hbus->bridge->dev, hbus->irq_domain);
 
 	return 0;
 }
@@ -3155,9 +3158,9 @@ static int hv_pci_probe(struct hv_device *hdev,
 		goto unmap;
 	}
 
-	hbus->sysdata.fwnode = irq_domain_alloc_named_fwnode(name);
+	hbus->fwnode = irq_domain_alloc_named_fwnode(name);
 	kfree(name);
-	if (!hbus->sysdata.fwnode) {
+	if (!hbus->fwnode) {
 		ret = -ENOMEM;
 		goto unmap;
 	}
@@ -3235,7 +3238,7 @@ exit_d0:
 free_irq_domain:
 	irq_domain_remove(hbus->irq_domain);
 free_fwnode:
-	irq_domain_free_fwnode(hbus->sysdata.fwnode);
+	irq_domain_free_fwnode(hbus->fwnode);
 unmap:
 	iounmap(hbus->cfg_addr);
 free_config:
@@ -3358,7 +3361,7 @@ static int hv_pci_remove(struct hv_device *hdev)
 	hv_free_config_window(hbus);
 	hv_pci_free_bridge_windows(hbus);
 	irq_domain_remove(hbus->irq_domain);
-	irq_domain_free_fwnode(hbus->sysdata.fwnode);
+	irq_domain_free_fwnode(hbus->fwnode);
 	put_hvpcibus(hbus);
 	wait_for_completion(&hbus->remove_event);
 
