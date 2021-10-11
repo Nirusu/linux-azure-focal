90d210d2e18860e16f0a75113cd64c9f969649a9
Tianyu Lan <Tianyu.Lan@microsoft.com>
2021-10-11 11:47:33 -0400

    UBUNTU: SAUCE: vmbus: Fix reserve bounce buffer isn't released during unloading driver
    BugLink: https://bugs.launchpad.net/bugs/1915146
    
    This patch is to fix issue during load/unload storvsc/netvsc driver.
    The reserved bounce buffer wasn't totally freed and related variables
    weren't reset completely.
    
    (cherry picked from commit
     https://github.com/lantianyu/linux/commit/7ff3a1434cd939ce3158ab38db20d25ed12aa396)
    [marcelo.cerri@canonical: minor context adjustments]
    Signed-off-by: Marcelo Henrique Cerri <marcelo.cerri@canonical.com>

--- drivers/net/hyperv/rndis_filter.c
+++ drivers/net/hyperv/rndis_filter.c
@@ -1277,7 +1277,8 @@ static void netvsc_sc_open(struct vmbus_channel *new_sc)
 	if (atomic_inc_return(&nvscdev->open_chn) == nvscdev->num_chn)
 		wake_up(&nvscdev->subchan_open);
 
-	hv_bounce_resources_reserve(new_sc, 4 * PAGE_SIZE * 1024);
+	if (hv_bounce_resources_reserve(new_sc, 4 * PAGE_SIZE * 1024) < 0)
+		pr_err("Fail to reserve bounce buffer for subchannel.\n");
 }
 
 /* Open sub-channels after completing the handling of the device probe.
