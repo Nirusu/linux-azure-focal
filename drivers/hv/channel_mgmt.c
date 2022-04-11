79264fa222508b067d6ee4c95d28aefa6135bf8a
Marcelo Henrique Cerri <marcelo.cerri@canonical.com>
2022-04-11 09:36:22 -0300

    Revert "Drivers: hv: vmbus: Remove the target_vp field from the vmbus_channel struct"
    BugLink: http://bugs.launchpad.net/bugs/1968564
    
    This reverts commit a0f7ece168c5d8d35f799a398a5745ac0744926c.
    
    Signed-off-by: Marcelo Henrique Cerri <marcelo.cerri@canonical.com>

--- drivers/hv/channel_mgmt.c
+++ drivers/hv/channel_mgmt.c
@@ -710,6 +710,8 @@ static void init_vp_index(struct vmbus_channel *channel)
 		 */
 		channel->numa_node = cpu_to_node(VMBUS_CONNECT_CPU);
 		channel->target_cpu = VMBUS_CONNECT_CPU;
+		channel->target_vp =
+			hv_cpu_number_to_vp_number(VMBUS_CONNECT_CPU);
 		if (perf_chn)
 			hv_set_alloced_cpu(VMBUS_CONNECT_CPU);
 		return;
@@ -743,6 +745,7 @@ static void init_vp_index(struct vmbus_channel *channel)
 	cpumask_set_cpu(target_cpu, alloced_mask);
 
 	channel->target_cpu = target_cpu;
+	channel->target_vp = hv_cpu_number_to_vp_number(target_cpu);
 
 	free_cpumask_var(available_mask);
 }
