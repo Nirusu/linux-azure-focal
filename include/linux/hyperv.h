6744c9e83b3904344f46e14c29ce22d98ad0b4ac
Michael Kelley <mikelley@microsoft.com>
2022-05-25 08:37:39 -0600

    Drivers: hv: vmbus: Rework use of DMA_BIT_MASK(64)
    BugLink: https://bugs.launchpad.net/bugs/1975717
    
    Using DMA_BIT_MASK(64) as an initializer for a global variable
    causes problems with Clang 12.0.1. The compiler doesn't understand
    that value 64 is excluded from the shift at compile time, resulting
    in a build error.
    
    While this is a compiler problem, avoid the issue by setting up
    the dma_mask memory as part of struct hv_device, and initialize
    it using dma_set_mask().
    
    Reported-by: Nathan Chancellor <nathan@kernel.org>
    Reported-by: Vitaly Chikunov <vt@altlinux.org>
    Reported-by: Jakub Kicinski <kuba@kernel.org>
    Fixes: 743b237c3a7b ("scsi: storvsc: Add Isolation VM support for storvsc driver")
    Signed-off-by: Michael Kelley <mikelley@microsoft.com>
    Reviewed-by: Nathan Chancellor <nathan@kernel.org>
    Tested-by: Nathan Chancellor <nathan@kernel.org>
    Link: https://lore.kernel.org/r/1644176216-12531-1-git-send-email-mikelley@microsoft.com
    Signed-off-by: Wei Liu <wei.liu@kernel.org>
    (cherry picked from commit 6bf625a4140f24b490766043b307f8252519578b)
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Philip Cox <philip.cox@canonical.com>
    Acked-by: Cengiz Can <cengiz.can@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- include/linux/hyperv.h
+++ include/linux/hyperv.h
@@ -1292,6 +1292,7 @@ struct hv_device {
 	struct vmbus_channel *channel;
 	struct kset	     *channels_kset;
 	struct device_dma_parameters dma_parms;
+	u64 dma_mask;
 
 	/* place holder to keep track of the dir for hv device in debugfs */
 	struct dentry *debug_dir;
